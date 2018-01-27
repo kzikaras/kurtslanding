8496bdf36dfbbefb713883778dfda1423ba6af24
Kurt Zikaras <kurtzikaras@gmail.com>
2018-01-27 18:14:40 -0500

    load new images

--- js/index.js
+++ js/index.js
@@ -1,24 +1,19 @@
-window.onload = function(){
-  console.log("loaded");
-}
+var animateThis = $('html, body');
 
 $("#about-button").click(function() {
-  console.log("#about-button clicked");
-  $('html, body').animate({
+  animateThis.animate({
     scrollTop: $('#about-section').offset().top
   }, 1900);
-});
+})
 
 $("#portfolio-button").click(function() {
-  console.log("#portfolio-button clicked");
-  $('html, body').animate({
+  animateThis.animate({
     scrollTop: $('#portfolio-section').offset().top
   }, 1900);
 })
 
 $("#contact-button").click(function() {
-  console.log("#contact-button clicked");
-  $('html, body').animate({
+  animateThis.animate({
     scrollTop: $('#contact-section').offset().top
   }, 1900);
 })
